0d5d9d854fbcfd007947c712fd6b83830f7daf14
Charnel Clamosa <charnel.clamosa16@gmail.com>
2024-07-16 17:53:06 +0800

    Sigma.js and dayjs is working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-import { dayjs } from './dayjs/esm/index.d.ts'
+import dayjs from 'dayjs';
+import Sigma from "sigma"
+import Graph from "graphology";
 
 console.log(dayjs('2018-08-08'))
+
+const graph = new Graph();
+graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
+graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
+graph.addEdge("1", "2", { size: 5, color: "purple" });
+
+// Instantiate sigma.js and render the graph
+const sigmaInstance = new Sigma(graph, document.getElementById("container"));
